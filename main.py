39aa1557de5a26f24ded33ba96fac991af603a74
Sean Smith <sean.smith@paladin.insure>
2020-01-29 18:30:49 -0800

    completed buttons pressed

--- main.py
+++ main.py
@@ -193,7 +193,7 @@ if __name__== "__main__":
     def buttonsPressed(buttonArray):
         if (buttonArray[0] == 'start'):
             btn = buttonConverter['start']
-            print(btn.read())
+            print('start button is ', btn.read())
         for buttonName in buttonArray:
             try:
                 button = buttonConverter[buttonName]
