eab7082197f3af2c082a6bb567bc8cba0226be1c
Sean Smith <sean.smith@paladin.insure>
2020-01-31 18:28:08 -0800

    cleanup

--- test.py
+++ test.py
@@ -1,20 +1,3 @@
 from shared.display import gameDisplay
 
-buttonConverter = {
-    blue: "arduino.get_pin('d:4:i')",
-    yellow: "arduino.get_pin('d:12:i')",
-    start: "arduino.get_pin('d:6:i')",
-    restart: "arduino.get_pin('d:5:i')",
-    left: "arduino.get_pin('d:10:i')",
-    right: "arduino.get_pin('d:9:i')",
-    up: "arduino.get_pin('d:8:i')",
-    down: "arduino.get_pin('d:7:i')"
-}
-
-print(buttonConverter.blublue'])
-
-
-arr = ['a', 'b', 'c', 'd']
-
-for var in arr:
-  print(var)
+print('success')
