5cc09bd2fe2480fd4d313c7d02efb6c0f4507dd5
Sean Smith <sean.smith@paladin.insure>
2020-01-31 19:25:56 -0800

    regular

--- ngini.py
+++ ngini.py
@@ -39,7 +39,31 @@ lights = {
 }
 
 ##### BUTTONS #####
-from shared.buttons import buttonsPressed, BUTTON_WIDTH, BUTTON_HEIGHT, BUTTON_CENTER_ONE_THIRD, BUTTON_CENTER_TWO_THIRD, BUTTON_CENTER_VERTICAL
+from shared.buttons import BUTTON_WIDTH, BUTTON_HEIGHT, BUTTON_CENTER_ONE_THIRD, BUTTON_CENTER_TWO_THIRD, BUTTON_CENTER_VERTICAL
+buttons = {
+  'blue': arduino.get_pin('d:4:i'),
+  'yellow': arduino.get_pin('d:12:i'),
+  'start': arduino.get_pin('d:6:i'),
+  'restart': arduino.get_pin('d:5:i'),
+  'left': arduino.get_pin('d:10:i'),
+  'right': arduino.get_pin('d:9:i'),
+  'up': arduino.get_pin('d:8:i'),
+  'down': arduino.get_pin('d:7:i')
+}
+
+BUTTON_PRESSED = False
+def buttonsPressed(buttonArray):
+    if (buttonArray[0] == 'start'):
+        btn = buttons['start']
+        print('start button is ', btn.read())
+    for buttonName in buttonArray:
+        try:
+            button = buttons[buttonName]
+        except:
+            return False
+        if (button.read() != BUTTON_PRESSED):
+            return False
+    return True
 
 ###### SOUNDS #####
 from shared.sounds import soundMissile, soundSuccess, lava, gamePlayMusic
