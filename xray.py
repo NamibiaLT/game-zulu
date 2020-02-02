9891d8d7542cde00f5d043c65f941dfc3aad4f24
James Bell <jamesleonbell@gmail.com>
2020-02-01 18:37:21 -0800

    changes

---
+++ xray.py
@@ -0,0 +1,4 @@
+#Game Details:
+# lights1,2,3,4,5 will sequence when won
+# Must push button1,2,3 in the correct sequence
+# lights1,2,3,4,5 will come on consequitively when correct sequence is pushed.
