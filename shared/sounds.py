0d797d8c29e853dd43f0c0c2d586a8a56e766c29
James Bell <jamesleonbell@gmail.com>
2020-02-02 14:49:01 -0800

    changes

--- shared/sounds.py
+++ shared/sounds.py
@@ -10,6 +10,7 @@ soundSuccess = pygame.mixer.Sound("sounds/success.wav")
 soundTrumpet = pygame.mixer.Sound("sounds/trumpet.wav")
 soundGateSuccess = pygame.mixer.Sound("sounds/sound_gate_success.wav") 
 soundButtonPushDead = pygame.mixer.Sound("sounds/button_push_dead.wav")
+soundButtonDead = pygame.mixer.Sound("sounds/button-10.wav")
 soundButtonPush1 = pygame.mixer.Sound("sounds/button_push_1.wav")
 soundbuttonPush2 = pygame.mixer.Sound("sounds/button_push_2.wav")
 
