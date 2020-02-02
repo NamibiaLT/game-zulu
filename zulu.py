03e0a6119658bac4f44fb522bc78c5b08c62763d
James Bell <jamesleonbell@gmail.com>
2020-02-02 14:47:26 -0800

    changes

--- zulu.py
+++ zulu.py
@@ -32,7 +32,7 @@ arduino = getArduino()
 
 from shared.color import BLACK, WHITE, RED, GREEN, BRIGHT_RED, BRIGHT_GREEN
 from shared.text import text_objects
-from shared.sounds import soundMissile, soundSuccess, gamePlayMusic,, soundTrumpet, introMusicSpace, soundButtonPushDead, soundButtonPush1, soundbuttonPush2, soundGateSuccess, 
+from shared.sounds import soundMissile,soundGateSuccess, soundSuccess, gamePlayMusic, soundTrumpet, introMusicSpace, soundButtonPushDead, soundButtonPush1, soundbuttonPush2
 
 ##### LIGHTS #####
 lights = {
