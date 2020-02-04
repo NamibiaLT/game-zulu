9d84cc399f02181948a3ce8c1baa678da518f44e
James Bell <jamesleonbell@gmail.com>
2020-02-03 18:22:48 -0800

    changes

--- shared/arduino_setup.py
+++ shared/arduino_setup.py
@@ -11,27 +11,15 @@ def getArduino():
       'disabled' : (0, 1, 14, 15) # Rx, Tx, Crystal
   }
 
-try:
-    port = '/dev/ttyACM0'
-except NameError:
-    port = '/dev/ttyACM1'
-except AttributeError:
-    port = 'COM7'
-except:
-    print("No board is detected\n")
 
-
-arduino = Arduino(port, mega, 57600)
-
-
-#   try:
-#       arduino = Arduino('/dev/ttyACM0', mega, 57600)
-#   except NameError:
-#       arduino = Arduino('/dev/ttyACM1', mega, 57600)
-#   except AttributeError:
-#       arduino = Arduino('COM7', mega, 57600)
-#   except:
-#       print("No arduino board is detected\n")
+  try:
+      arduino = Arduino('/dev/ttyACM0', mega, 57600)
+  except NameError:
+      arduino = Arduino('/dev/ttyACM1', mega, 57600)
+  except AttributeError:
+      arduino = Arduino('COM7', mega, 57600)
+  except:
+      print("No arduino board is detected\n")
 
   iterator = util.Iterator(arduino)
   iterator.start()
